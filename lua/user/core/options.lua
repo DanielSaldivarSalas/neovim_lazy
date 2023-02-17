b751cc65527fde3dbf53015c371399fb85a4e1aa
Daniel Saldivar <daniel.saldivar.ds@gmail.com>
2023-02-17 17:33:20 -0500

    ci: added luacheck

---
+++ lua/user/core/options.lua
@@ -0,0 +1,16 @@
+--[[local options = {
+	--line numbers
+	number = true,
+	relativenumber = true,
+	autowrite = true,
+}
+]]
+
+local opt = vim.opt
+opt.number = true
+opt.number = true
+--vim.opt.autowrite = true
+
+--for k, v in pairs(options) do
+--	vim.opt[k] = v
+--end
